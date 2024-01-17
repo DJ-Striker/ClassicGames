77f35c371816e487397b605d4268785ede00f0cc
Mayukh <mayukhsaleel@gmail.com>
2024-01-18 00:01:57 +0530

    Final Changes

--- Rock-Paper-Scissors/rock-paper-scissors-master/app.js
+++ Rock-Paper-Scissors/rock-paper-scissors-master/app.js
@@ -4,9 +4,9 @@ setTimeout(() => {
 }, 500)
 
 // DOM
-const btnRules = document.querySelector('.rules-btn')
-const btnClose = document.querySelector('.close-btn')
-const modalRules = document.querySelector('.modal')
+const btnRules = document.querySelector('.rules-btn');
+const btnClose = document.querySelector('.close-btn');
+const modalRules = document.querySelector('.modal');
 
 const CHOICES = [
     {
@@ -21,16 +21,20 @@ const CHOICES = [
         name: "rock",
         beats: "scissors"
     }
-]
+];
 
-const choiceButtons = document.querySelectorAll('.choice-btn')
-const gameDiv = document.querySelector('.game')
-const resultsDiv = document.querySelector('.results')
-const resultDivs = document.querySelectorAll('.results_result')
+const choiceButtons = document.querySelectorAll('.choice-btn');
+const gameDiv = document.querySelector('.game');
+const resultsDiv = document.querySelector('.results');
+const resultDivs = document.querySelectorAll('.results_result');
 
-const resultWinner = document.querySelector('.results_winner')
-const resultText = document.querySelector('.results_text')
+const resultWinner = document.querySelector('.results_winner');
+const resultText = document.querySelector('.results_text');
 
+const playAgainBtn = document.querySelector('.play-again');
+
+const scoreNumber = document.querySelector('.score_number');
+let score = 0;
 // Game Logic
 choiceButtons.forEach( button => {
     button.addEventListener('click', () => {
@@ -58,32 +62,33 @@ function displayResults(results) {
             resultDiv.innerHTML =  `
             <div class="choice ${results[idx].name}">
                 <img src="images/icon-${results[idx].name}.svg" alt="${results[idx].name}" />
-            </div>      `
+            </div>      `;
                        
         }, idx * 1000);
     });
 
-    gameDiv.classList.toggle('hidden')
-    resultsDiv.classList.toggle('hidden')
+    gameDiv.classList.toggle('hidden');
+    resultsDiv.classList.toggle('hidden');
 }
 
 function displayWinner(results) {
     setTimeout(() => {
-        const userWins = isWinner(results)
-        const aiWins = isWinner(results.reverse())
+        const userWins = isWinner(results);
+        const aiWins = isWinner(results.reverse());
 
         if(userWins) {
-            resultText.innerText = "you win"
-            resultDivs[0].classList.toggle('winner')
+            resultText.innerText = "you win";
+            resultDivs[0].classList.toggle('winner');
+            keepScore(1)
         } else if (aiWins) {
-            resultText.innerText = "you lose"
-            resultDivs[1].classList.toggle('winner')
+            resultText.innerText = "you lose";
+            resultDivs[1].classList.toggle('winner');
         } else {
-            resultText.innerText = "draw"
+            resultText.innerText = "draw";
         }
 
-        resultWinner.classList.toggle('hidden')
-        resultsDiv.classList.toggle('show-winner')
+        resultWinner.classList.toggle('hidden');
+        resultsDiv.classList.toggle('show-winner');
         
     }, 1000);
 
@@ -93,6 +98,28 @@ function displayWinner(results) {
 function isWinner(results) {
     return results[0].beats === results[1].name;
 }
+
+//Score
+
+function keepScore(point){
+    score += point;
+    scoreNumber.innerText = score;
+}
+
+
+//Play Agian
+playAgainBtn.addEventListener('click', () => {
+    gameDiv.classList.toggle('hidden');
+    resultsDiv.classList.toggle('hidden');
+
+    resultDivs.forEach(resultDiv => {
+        resultDiv.innerHTML = "";
+        resultDiv.classList.remove('wiiner');
+    })
+    resultText.innerText = "";
+    resultWinner.classList.toggle('hidden');
+    resultsDiv.classList.toggle('show-winner');
+})
 
 //Show/Hide Rules
 
